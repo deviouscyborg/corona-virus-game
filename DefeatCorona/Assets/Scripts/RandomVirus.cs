a346c1ef43a0fd53eb8346c614497b46d17558b4
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-03-27 02:10:03 +0530

    added backgrounds, viruses, injections and score counter

---
+++ DefeatCorona/Assets/Scripts/RandomVirus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class RandomVirus : MonoBehaviour {
+ 
+     public Sprite[] Viruses; //this is an array of type sprite
+ 
+     public SpriteRenderer Render; //this is a variable of type sprite renderer
+ 
+     // Use this for initialization
+     void Start () {
+            Render = GetComponent<SpriteRenderer>(); 
+            /*assigning the Render to the object's SpriteRender, this will allow us to access the image from 
+            code*/
+            Render.sprite = Viruses[Random.Range(0, Viruses.Length)]; 
+            /*this will change the current sprite of the sprite renderer to a random sprite that was chosen 
+            randomly from the array of backgrounds */
+         }
+     // Update is called once per frame
+     void Update () {
+ 
+     }
+ }
---
+++ DefeatCorona/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	private int score;
+
+	public Text scoreText;
+
+ 	void Start(){
+     		score = 0;
+ 		}
+	
+
+		 
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = score.ToString();
+			if (Input.GetMouseButtonDown(0)) {
+			 score++;
+		 }
+		scoreText.text = score.ToString();
+	}
+}
