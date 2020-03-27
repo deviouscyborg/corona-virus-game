348d7a5fbc2357ce486fa20362f7ceec4ead0e02
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-03-27 23:27:04 +0530

    added plugin configured

---
+++ DefeatCorona/Assets/Scripts/AdManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+
+public class AdManager : MonoBehaviour
+{
+    private string APP_ID = "ca-app-pub-4500169197993541~1454919148";
+    private BannerView bannerAD;
+    private InterstitialAd interstitialAd;
+    private RewardBasedVideoAd rewardVideoAD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // MobileAds.Initialize(initStatus => { });
+
+        RequestBanner();
+        RequestInterstitial();
+        RequestVideoAd();
+
+    }
+
+    void RequestBanner() {
+        string banner_Id = "ca-app-pub-3940256099942544/6300978111";
+        bannerAD = new BannerView(banner_Id, AdSize.Banner, AdPosition.Top);
+
+        //for real
+        // AdRequest adRequest = new AdRequest.Builder().Build();
+
+        //for testing
+        AdRequest adRequest = new AdRequest.Builder()
+        .AddTestDevice("33BE2250B43518CCDA7DE426D04EE232").Build();
+
+        bannerAD.LoadAd(adRequest);
+    }
+
+
+    void RequestInterstitial() {
+        string interstitial_Id = "ca-app-pub-3940256099942544/1033173712";
+        interstitialAd = new InterstitialAd(interstitial_Id);
+
+        //for real
+        // AdRequest adRequest = new AdRequest.Builder().Build();
+
+        //for testing
+        AdRequest adRequest = new AdRequest.Builder()
+        .AddTestDevice("33BE2250B43518CCDA7DE426D04EE232").Build();
+
+        interstitialAd.LoadAd(adRequest);
+    }
+
+    void RequestVideoAd() {
+        string video_Id = "ca-app-pub-3940256099942544/5224354917";
+        rewardVideoAD = RewardBasedVideoAd.Instance;
+
+        //for real
+        // AdRequest adRequest = new AdRequest.Builder().Build();
+
+        //for testing
+        AdRequest adRequest = new AdRequest.Builder()
+        .AddTestDevice("33BE2250B43518CCDA7DE426D04EE232").Build();
+
+        rewardVideoAD.LoadAd(adRequest, video_Id);
+    }
+
+    public void Display_Banner() {
+        bannerAD.Show();
+    }
+
+    public void Display_Interstital() {
+        if(interstitialAd.IsLoaded()) {
+            interstitialAd.Show();
+        }
+    }
+
+    public void Display_Reward_Video() {
+        if(rewardVideoAD.IsLoaded()){
+            rewardVideoAD.Show();
+        }
+    }
+
+
+    //handle events
+
+     public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        Display_Banner();
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        RequestBanner();
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+
+
+    void HandleBannerAds(bool subscribe) {
+
+        if(subscribe) {
+             // Called when an ad request has successfully loaded.
+        this.bannerAD.OnAdLoaded += this.HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.bannerAD.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        this.bannerAD.OnAdOpening += this.HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        this.bannerAD.OnAdClosed += this.HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.bannerAD.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+        } else {
+             // Called when an ad request has successfully loaded.
+        this.bannerAD.OnAdLoaded -= this.HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.bannerAD.OnAdFailedToLoad -= this.HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        this.bannerAD.OnAdOpening -= this.HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        this.bannerAD.OnAdClosed -= this.HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.bannerAD.OnAdLeavingApplication -= this.HandleOnAdLeavingApplication;
+        }
+       
+    }
+
+    void OnEnable() {
+        HandleBannerAds(true);
+    }
+
+    void OnDisable() {
+        HandleBannerAds(false);
+    }
+}
